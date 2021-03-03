6b12fe502915fe655fa9f721a3c44d57a6c2c8ba
ChuprinaAnn <ann.skok77@gmail.com>
2021-03-03 19:12:46 +0300

    03.03.21 Сделала Triangles и Ray

--- src/main/java/problem/Triangle.java
+++ src/main/java/problem/Triangle.java
@@ -1,4 +1,34 @@
 package problem;
 
+import javax.media.opengl.GL2;
+
+import static javax.media.opengl.GL.GL_TRIANGLES;
+
 public class Triangle {
+    double ax;
+    double ay;
+    double bx;
+    double by;
+    double cx;
+    double cy;
+    public Triangle( double ax, double ay, double bx, double by, double cx, double cy) {
+        this.ax = ax;
+        this.ay = ay;
+        this.bx = bx;
+        this.by = by;
+        this.cx = cx;
+        this.cy = cy;
+    }
+    public void render(GL2 gl) {
+        gl.glBegin(GL_TRIANGLES);
+        gl.glVertex2d(ax, ay);
+        gl.glColor3d(1, 0, 1);
+        gl.glVertex2d(bx, by);
+        gl.glColor3d(1, 1, 1);
+        gl.glVertex2d(cx, cy);
+        gl.glColor3d(0, 1, 1);
+        gl.glEnd();
+
+    }
+
 }
